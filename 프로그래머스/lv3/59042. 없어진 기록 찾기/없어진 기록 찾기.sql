e3dc89758b098d8428a794e092bf947f1cf6d184
Kim Su-Bin <43884708+tabss2003@users.noreply.github.com>
2023-06-05 22:40:53 +0900

    Update 없어진 기록 찾기.sql

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,6 +1,10 @@
 -- 코드를 입력하세요
-select outs.animal_id ,outs.name
+# select outs.animal_id ,outs.name
+# from animal_outs as outs
+# left join animal_ins as ins
+# on outs.animal_id = ins.animal_id
+# where ins.animal_id IS NULL;
+
+select outs.animal_id, outs.name
 from animal_outs as outs
-left join animal_ins as ins
-on outs.animal_id = ins.animal_id
-where ins.animal_id IS NULL;
+where outs.animal_id not in (select ins.animal_id from animal_ins as ins);
