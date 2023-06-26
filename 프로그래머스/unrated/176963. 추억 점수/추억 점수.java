fa22655a3132799d18a8d8031078d5c315dd329f
tabss2003 <tabss2003@naver.com>
2023-06-26 22:50:00 +0900

    Update : 추억점수

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -1,13 +1,36 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
 
-        for(int i=0;i< photo.length;i++){
-            for(int j=0;j<photo[i].length;j++){
-                for(int p=0;p<name.length;p++){
-                    if(photo[i][j].equals(name[p])) answer[i]+=yearning[p];
-                }
+        /** 삼중 for문*/
+//        for(int i=0;i< photo.length;i++){
+//            for(int j=0;j<photo[i].length;j++){
+//                for(int p=0;p<name.length;p++){
+//                    if(photo[i][j].equals(name[p])) answer[i]+=yearning[p];
+//                }
+//            }
+//        }
+
+        /**HashMap*/
+        HashMap<String,Integer> map = new LinkedHashMap<>();
+
+        for(int i=0;i<name.length;i++){
+            map.put(name[i],yearning[i]);
+        }
+
+        for(int i=0;i<photo.length;i++){
+            String[] persons = photo[i];
+            int score =0;
+
+            for(int j=0;j< persons.length;j++){
+                String person = persons[j];
+                if(map.containsKey(person)) score+=map.get(person);
             }
+
+            answer[i]= score;
         }
         return answer;
     }
